6455e49dd9e7c6f7fa5e9c45c3d40bd7de468995
willpires <william.joga91@gmail.com>
2023-02-22 13:35:35 -0300

    ajuste componet ,menu_header

--- nava_flutter/lib/feature/home/presentation/widget/menu/menu_header.dart
+++ nava_flutter/lib/feature/home/presentation/widget/menu/menu_header.dart
@@ -3,17 +3,51 @@ import 'package:flutter/material.dart';
 class HeaderMenu extends StatelessWidget {
   const HeaderMenu({super.key});
 
+  final perfil = 'assets/images/perfil.png';
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.orange,
-      child: const ListTile(
-        leading: FlutterLogo(
-          size: 56.0,
-        ),
-        title: Text("Bem vindo"),
-        subtitle: Text("Caio Maximo"),
+      height: 10,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text("Olá",
+              style: TextStyle(
+              color: Colors.white
+          )),
+          Row(
+            children: [
+              Image.asset(perfil, width: 40,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  color: Colors.red,
+                  child: Column(children: [
+                    const Text("CAIO MAXIMO",
+                       style: TextStyle(
+                         fontSize: 15,
+                        color: Colors.white,
+                         fontWeight:FontWeight.bold ,
+
+                    ),),
+                    Text("Minha conta",
+
+                        style:TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                        fontWeight:FontWeight.bold
+                    ))
+                  ],),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
--- nava_flutter/lib/feature/home/presentation/widget/menu/menu_header_bottom.dart
+++ nava_flutter/lib/feature/home/presentation/widget/menu/menu_header_bottom.dart
@@ -3,6 +3,7 @@ import 'package:nava_flutter/core/util/degrader.dart';
 
 class MenuHeaderBottom extends StatelessWidget {
   const MenuHeaderBottom({Key? key}) : super(key: key);
+  final perfil = 'assets/images/perfil.png';
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +12,12 @@ class MenuHeaderBottom extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(40.0),
         child: Column(
-          children: const [
-            Icon(Icons.phone_android),
-            Text(
-              "Duvidas? Gasrnta agora o seu cartao",
+          children: [
+            Image.asset(perfil,height: 40,),
+            const Divider(height: 10,color: Colors.transparent,),
+            const Text(
+              "Duvidas? Gasrnta agora o seu cartao.",
+              textAlign: TextAlign.center,
               style: TextStyle(color: Colors.white),
             )
           ],
